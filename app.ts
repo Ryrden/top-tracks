e73143113a3c6b968de0d6e7b44a19d3f1ba4bf1
Ryan S <ryansateles@gmail.com>
2023-01-23 19:18:18 -0300

    change port

--- app.ts
+++ app.ts
@@ -10,7 +10,7 @@ import { Request, Response } from "express";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + "/public"))
     .use(cors())
