d7f4839031aa6543bd481e01ba307c4e9f08110b
Ryan S <ryanssteles@gmail.com>
2023-10-11 00:39:39 -0300

    build: trying vercel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './Components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     screens: {
