1908ebcbfa1e6f3b8317879cc65a4aa35cb2919b
Ryan S <ryanssteles@gmail.com>
2023-10-11 16:44:22 -0300

    here we go again

--- next.config.js
+++ next.config.js
@@ -4,11 +4,13 @@ const nextConfig = {
         CLIENT_ID: process.env.CLIENT_ID,
         CLIENT_SECRET: process.env.CLIENT_SECRET,
         REDIRECT_URI: process.env.REDIRECT_URI,
+        PORT: process.env.PORT,
+        ENV: process.env.ENV,
     },
     images: {
         domains: ['i.scdn.co'],
     },
-    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://top-tracks-spotify.vercel.app/' : 'http://localhost:5000'
+    assetPrefix: process.env.ENV === 'production' ? 'https://top-tracks-spotify.vercel.app/' : `http://localhost:${process.env.PORT || 3000}`
 }
 
 module.exports = nextConfig
